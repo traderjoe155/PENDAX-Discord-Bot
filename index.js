67fe061a4bc3d161adaf299fdb1ae2826bcdf2dd
traderjoe155 <traderjoe155@gmail.com>
2023-01-07 12:48:58 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,80 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+import axios from "axios";
+import { createExchange } from "pendax/package/exchanges/exchange.js";
+
+let discHook = process.env.DISCHOOK;
+let publicExchange = createExchange({
+  exchange: "okx",
+  authenticate: false,
+  label: "okx",
+});
+let timeDelay = process.env.TIMEDELAY;
+
+// let privateExchange = createExchange({
+//         exchange: "okx",
+//         authenticate: true,
+//         key: process.env.KEY,
+//         secret: process.env.SECRET,
+//         passphrase: process.env.PASSPHRASE,
+//         label: "okxPriv",
+//         marginType: "usdt"
+//     });
+
+async function getOpenInterest(publicExchange, options) {
+  try {
+    let result = await publicExchange.getOpenInterest(options);
+    return result;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+//sendDiscordPost//
+async function sendDiscordPost(options) {
+  try {
+    let api = discHook;
+    let content = options;
+    let params = {
+      content: content,
+    };
+
+    await axios({
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: JSON.stringify(params),
+      url: api,
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+async function main(i) {
+  let result = await getOpenInterest(publicExchange, {
+    instType: process.env.INSTTYPE,
+    instId: process.env.INSTID,
+  });
+  let openInterest = result.data.data[0].oi;
+  let oiFormatted = parseFloat(openInterest).toLocaleString();
+  let market = result.data.data[0].instId;
+  console.log(market);
+  console.log(oiFormatted);
+  let data =
+    "`" +
+    new Date().toLocaleString() +
+    "`" +
+    "\n**Market:** " +
+    market +
+    "\n**OpenInterest:** " +
+    oiFormatted;
+  await sendDiscordPost(data);
+  console.log(data);
+  setTimeout(() => {
+    main(++i);
+  }, timeDelay);
+}
+
+main(0);
